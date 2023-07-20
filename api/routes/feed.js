4b38a94508e8a929b27714b7d701bcd3e5a1da62
niklonskyi <niklonskyi@icloud.com>
2023-07-21 00:05:42 +0300

    feat: Add validation for POST request

--- api/routes/feed.js
+++ api/routes/feed.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { body } from "express-validator";
 
 import * as feedController from "../controllers/feed.js";
 
@@ -7,6 +8,18 @@ const feedRouter = express.Router();
 // GET /feed/posts
 feedRouter.get("/posts", feedController.getPosts);
 
-feedRouter.post('/post', feedController.createPost)
+// POST /feed/post
+feedRouter.post(
+  "/post",
+  [
+    body("title")
+      .trim()
+      .isLength({ min: 5 }),
+    body("content")
+      .trim()
+      .isLength({ min: 5 }),
+  ],
+  feedController.createPost
+);
 
 export default feedRouter;
