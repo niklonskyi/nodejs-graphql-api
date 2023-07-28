220877713695e01b1e0ab99ffedfb23559988f60
niklonskyi <niklonskyi@icloud.com>
2023-07-28 13:56:58 +0300

    feat: User model

---
+++ api/models/user.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: 'I am new!'
+  },
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Post" }], 
+});
+
+export default mongoose.model('User', userSchema);
